056883cfcb96db543ca42059a0d07708de7f9088
Udhayaboopathi V <udhayaboopathi2003@gmail.com>
2025-07-20 14:38:51 +0000

    i need three option light , dreak , system theam

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -21,8 +21,8 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
-          enableSystem={false}
+          defaultTheme="system"
+          enableSystem
           disableTransitionOnChange
         >
           {children}
