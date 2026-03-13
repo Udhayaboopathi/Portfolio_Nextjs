df304dca451f63cef685e44cad5524931423d58e
Udhayaboopathi <udhayaboopathi2003@gmail.com>
2026-03-14 04:20:22 +0530

    fix: upgrade Node.js version to 20-alpine and update Next.js dependencies

--- next.config.js
+++ next.config.js
@@ -1,25 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output: "standalone",
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
   experimental: {
     serverActions: {
-      allowedOrigins: [
-        'localhost:9002',
-        '*.app.github.dev',
-      ],
+      allowedOrigins: ["localhost:9002", "*.app.github.dev"],
     },
   },
 };
